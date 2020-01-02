8bee066286e4ebdcb7f8474e7e898822cd757fbb
Alireza Jahandideh <ar.jahandideh@gmail.com>
2020-01-02 02:36:50 -0600

    add dist to repo

---
+++ dist/precache-manifest.3441a3c6bc047abcc24364069d0f93d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46eeab6c41725c502908",
+    "url": "/css/app.0bfc9e82.css"
+  },
+  {
+    "revision": "f0c981969905dbf97920b8e20c8ef25a",
+    "url": "/img/logo.f0c98196.svg"
+  },
+  {
+    "revision": "10cb7899cc1ee3d5ac6dd2ec80a06dd8",
+    "url": "/img/peek.10cb7899.svg"
+  },
+  {
+    "revision": "bafb2836a9f3d787cfec949f44359c65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46eeab6c41725c502908",
+    "url": "/js/app.89a6b9eb.js"
+  },
+  {
+    "revision": "5dd77493bf345e4055dd",
+    "url": "/js/chunk-vendors.d1a77d81.js"
+  },
+  {
+    "revision": "2b3935ef4fefb11de346c3e087a593c3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
